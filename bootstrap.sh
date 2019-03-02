a2d7483b28f8ff43d784ee166815536ed177bbda
Francesc-Mestre <fmestre@voxelroup.net>
2019-03-02 11:57:42 +0100

    Primer Commit

---
+++ bootstrap.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+mkdir -m 0700 /root/.ssh
+cp /vagrant/id_ed25519.pub /root/.ssh/authorized_keys
+cat /vagrant/id_ed25519.pub >> /home/vagrant/.ssh/authorized_keys
+chmod 600 /root/.ssh/authorized_keys
